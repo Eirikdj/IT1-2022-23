8a9612e34a200a0e32633dc0685bf03fe729edb9
Eirikdj <113419392+Eirikdj@users.noreply.github.com>
2023-05-16 10:17:44 +0200

    Add files via upload

---
+++ eirik.js
@@ -0,0 +1,7 @@
+let data = {
+    "kortArray" :[
+        { "navn": "Atle Skjelbred", "stilling": "Elev", "Dato": "2005-12-26", "Kjonn": "Mann", "tlf": "48381518", "Forerkort": "Har ikke fører kort", "Ramme": "5px"},
+        { "navn": "Ask Jakob Ødegård Johansen", "stilling": "Elev", "Dato": "2005-09-02", "Kjonn": "Mann", "tlf": "48381518", "Forerkort": "Har ikke fører kort", "Ramme": "5px"},
+        { "navn": "Sindre Knap ", "stilling": "Elev", "Dato": "1492-01.01", "Kjonn": "Mann", "tlf": "48381518", "Forerkort": "Har ikke fører kort", "Ramme": "5px"},
+        { "navn": "Eirik Djup", "stilling": "SFO-arbeider", "Dato": "2005-06-13", "Kjonn": "Mann", "tlf": "48381518", "Forerkort": "Har ikke fører kort", "Ramme": "5px"}
+    ]};
